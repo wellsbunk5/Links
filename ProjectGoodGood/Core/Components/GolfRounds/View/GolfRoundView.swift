3dc345eb048bef56848c7f323b9e3667b2362555
Nathan Sonnenberg <ntsonnenberg@gmail.com>
2023-02-07 17:24:08 -0700

    Color Extension created with color codes.

--- ProjectGoodGood/Core/Components/GolfRounds/View/GolfRoundView.swift
+++ ProjectGoodGood/Core/Components/GolfRounds/View/GolfRoundView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 import Kingfisher
 
-extension Color {
-    static let birdyColor = Color(red: 0.286, green: 0.592, blue: 0.345)
-    static let parColor = Color(red: 0.863, green: 0.643, blue: 0.278)
-    static let bogeyColor = Color(red: 0.576, green: 0.439, blue: 0.2)
-    
-    static let roundColor = Color(red: 0.898, green: 0.898, blue: 0.898)
-}
+//extension Color {
+//    static let birdyColor = Color(red: 0.286, green: 0.592, blue: 0.345)
+//    static let parColor = Color(red: 0.863, green: 0.643, blue: 0.278)
+//    static let bogeyColor = Color(red: 0.576, green: 0.439, blue: 0.2)
+//    
+//    static let roundColor = Color(red: 0.898, green: 0.898, blue: 0.898)
+//}
 
 struct GolfRoundView: View {
     @ObservedObject var viewModel: GolfRoundViewModel
---
+++ ProjectGoodGood/Extensions/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  ProjectGoodGood
+//
+//  Created by Brooke Leonhardt on 2/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let birdyColor = Color(red: 0.286, green: 0.592, blue: 0.345)
+    static let parColor = Color(red: 0.863, green: 0.643, blue: 0.278)
+    static let bogeyColor = Color(red: 0.576, green: 0.439, blue: 0.2)
+    
+    static let roundColor = Color(red: 0.898, green: 0.898, blue: 0.898)
+}
