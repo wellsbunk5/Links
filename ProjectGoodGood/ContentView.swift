b2e407c9a34cd6d53d9573a4620355f7332dcbf3
Wells Bunker <wellsbunk5@gmail.com>
2023-03-17 23:02:02 -0600

    Just in case

--- ProjectGoodGood/ContentView.swift
+++ ProjectGoodGood/ContentView.swift
@@ -6,64 +6,19 @@
 //
 
 import SwiftUI
-import Kingfisher
 
 struct ContentView: View {
-    @State private var showMenu = false
     @EnvironmentObject var viewModel: AuthViewModel
-    @ObservedObject var playRoundViewModel = PlayRoundViewModel()
     
     var body: some View {
-        if $viewModel.userSession == nil {
+        if viewModel.userSession == nil {
             NavigationStack(path: $viewModel.loginPresentedViews) {
                 LoginView2()
             }
             
         } else {
-            NavigationStack(path: $playRoundViewModel.playRoundPresentedViews) {
-                ZStack(alignment: .topLeading) {
-                    MainTabView(playRoundViewModel: playRoundViewModel)
-                        .navigationBarHidden(showMenu)
-                    
-                    if showMenu {
-                        ZStack {
-                            Color(.black)
-                                .opacity(showMenu ? 0.25 : 0.0)
-                        }.onTapGesture {
-                            withAnimation(.easeInOut) {
-                                showMenu = false
-                            }
-                        }
-                        .ignoresSafeArea()
-                    }
-                    
-                    SideMenuView()
-                        .frame(width: 300)
-                        .background(showMenu ? Color.white : Color.clear)
-                        .offset(x: showMenu ?  100 : 500, y: 0) // was 0 : -300, y: 0: changing it made it right aligned
-                }
-
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        if let user = viewModel.currentUser {
-                            Button {
-                                withAnimation(.easeInOut) {
-                                    showMenu.toggle()
-                                }
-                            } label: {
-                                KFImage(URL(string: user.profileImageUrl))
-                                    .resizable()
-                                    .scaledToFill()
-                                    .clipShape(Circle())
-                                    .frame(width: 32, height: 32)
-                            }
-                        }
-   
-                    }
-                }
-                .onAppear {
-                    showMenu = false
-            }
+            ZStack(alignment: .topLeading) {
+                MainTabView()
             }
         }
     }
--- ProjectGoodGood/Core/Feed/Views/FeedView.swift
+++ ProjectGoodGood/Core/Feed/Views/FeedView.swift
@@ -6,21 +6,67 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct FeedView: View {
-    @ObservedObject var viewModel = FeedViewModel()
+    @State private var showMenu = false
+    @EnvironmentObject var viewModel: AuthViewModel
+    @ObservedObject var feedViewModel = FeedViewModel()
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach ( viewModel.rounds) { golfRound in
-                    GolfRoundView(golfRound: golfRound)
-                        .padding()
-                    
+        NavigationStack {
+            ZStack(alignment: .topLeading) {
+                ScrollView {
+                    LazyVStack {
+                        ForEach ( feedViewModel.rounds) { golfRound in
+                            GolfRoundView(golfRound: golfRound)
+                                .padding()
+                            
+                        }
+                    }
+                }
+                .refreshable {
+                    feedViewModel.fetchRounds()
+                }
+                if showMenu {
+                    ZStack {
+                        Color(.black)
+                            .opacity(showMenu ? 0.25 : 0.0)
+                    }.onTapGesture {
+                        withAnimation(.easeInOut) {
+                            showMenu = false
+                        }
+                    }
+                    .ignoresSafeArea()
+                }
+                
+                SideMenuView()
+                    .frame(width: 300)
+                    .background(showMenu ? Color.white : Color.clear)
+                    .offset(x: showMenu ?  100 : 500, y: 0) // was 0 : -300, y: 0: changing it made it right aligned
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if let user = viewModel.currentUser {
+                        Button {
+                            withAnimation(.easeInOut) {
+                                showMenu.toggle()
+                            }
+                        } label: {
+                            KFImage(URL(string: user.profileImageUrl))
+                                .resizable()
+                                .scaledToFill()
+                                .clipShape(Circle())
+                                .frame(width: 32, height: 32)
+                        }
+                    }
+
                 }
             }
+            .onAppear {
+                showMenu = false
+            }
         }
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
